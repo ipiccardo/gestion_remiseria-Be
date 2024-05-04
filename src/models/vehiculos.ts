78789ff6797eea00801dcb9e15cd714b5b787dd3
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-05-04 20:42:46 -0300

    Se agrega modelo en el models de editar vehiculo

--- src/models/vehiculos.ts
+++ src/models/vehiculos.ts
@@ -4,158 +4,161 @@ import { Vehiculo } from "../utils/types";
 import { ViajeModel } from "./viajes";
 
 export class VehiculosModel {
-    static estadoVehiculo = async (idVehiculo: number) => {
-        try {
-            const estadoVehiculo = await pool.query(
-                "SELECT estado FROM vehiculo WHERE id = $1",
-                [idVehiculo]
-            );
+  static estadoVehiculo = async (idVehiculo: number) => {
+    try {
+      const estadoVehiculo = await pool.query(
+        "SELECT estado FROM vehiculo WHERE id = $1",
+        [idVehiculo]
+      );
 
-            return estadoVehiculo.rows[0].estado;
-        } catch (error) {
-            throw error;
-        }
-    };
+      return estadoVehiculo.rows[0].estado;
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    static actualizarKilometraje = async (
-        kilometrosRecorridos: number,
-        idVehiculo: number
-    ) => {
-        try {
-            const editarVehiculo = await pool.query(
-                `
+  static actualizarKilometraje = async (
+    kilometrosRecorridos: number,
+    idVehiculo: number
+  ) => {
+    try {
+      const editarVehiculo = await pool.query(
+        `
                 UPDATE vehiculo SET kilometros_restantes = kilometros_restantes + $1 WHERE vehiculo.id = $2 RETURNING kilometros_restantes
                 `,
-                [kilometrosRecorridos, idVehiculo]
-            );
+        [kilometrosRecorridos, idVehiculo]
+      );
 
-            if (editarVehiculo.rows[0].kilometros_restantes >= 15000) {
-                const desabilitarVehiculo = await pool.query(
-                    `
+      if (editarVehiculo.rows[0].kilometros_restantes >= 15000) {
+        const desabilitarVehiculo = await pool.query(
+          `
                     UPDATE vehiculo SET estado = $1, kilometros_restantes = $2, kilometraje = kilometraje + $3 WHERE vehiculo.id = $4
                     `,
-                    [false, 0, editarVehiculo.rows[0].kilometros_restantes, idVehiculo]
-                );
+          [false, 0, editarVehiculo.rows[0].kilometros_restantes, idVehiculo]
+        );
 
-                if (desabilitarVehiculo.rowCount) {
-                    return { ...Success.EDIT };
-                } else {
-                    return Fail.NOT_FOUND;
-                }
-            }
-
-            if (editarVehiculo.rowCount) {
-                return { ...Success.EDIT };
-            } else {
-                return Fail.NOT_FOUND;
-            }
-        } catch (error) {
-            throw error;
+        if (desabilitarVehiculo.rowCount) {
+          return { ...Success.EDIT };
+        } else {
+          return Fail.NOT_FOUND;
         }
-    };
+      }
 
-    static getVehiculos = async () => {
-        try {
-            const vehiculos = await pool.query(
-                `SELECT empleado.nombre, empleado.apellido, vehiculo.marca, vehiculo.kilometraje, vehiculo.estado, vehiculo.modelo, vehiculo.dominio, vehiculo.anio, vehiculo.id, empleado.id as id_empleado
+      if (editarVehiculo.rowCount) {
+        return { ...Success.EDIT };
+      } else {
+        return Fail.NOT_FOUND;
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  static getVehiculos = async () => {
+    try {
+      const vehiculos = await pool.query(
+        `SELECT empleado.nombre, empleado.apellido, vehiculo.marca, vehiculo.kilometraje, vehiculo.estado, vehiculo.modelo, vehiculo.dominio, vehiculo.anio, vehiculo.id, empleado.id as id_empleado
                 FROM vehiculo 
                 LEFT JOIN empleado ON vehiculo.id_empleado = empleado.id `
-            );
-            return { ...Success.GET, VEHICULOS: vehiculos.rows };
-        } catch (error) {
-            throw error;
-        }
-    };
+      );
+      return { ...Success.GET, VEHICULOS: vehiculos.rows };
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    static getVehiculosPorEmpleado = async (idEmpleado: number) => {
-        try {
-            const vehiculosPorEmpleado = await pool.query(
-                `SELECT vehiculo.marca, vehiculo.kilometraje, vehiculo.modelo, vehiculo.dominio, vehiculo.id 
+  static getVehiculosPorEmpleado = async (idEmpleado: number) => {
+    try {
+      const vehiculosPorEmpleado = await pool.query(
+        `SELECT vehiculo.marca, vehiculo.kilometraje, vehiculo.modelo, vehiculo.dominio, vehiculo.id 
                 FROM vehiculo 
                 INNER JOIN empleado ON vehiculo.id_empleado = empleado.id 
                 WHERE empleado.id = $1  `,
-                [idEmpleado]
-            );
+        [idEmpleado]
+      );
 
-            if (!!vehiculosPorEmpleado.rowCount) {
-                return { ...Success.GET, vehiculos: vehiculosPorEmpleado.rows };
-            }
-            return { ...Fail.NOT_FOUND };
-        } catch (error) {
-            throw error;
-        }
-    };
+      if (!!vehiculosPorEmpleado.rowCount) {
+        return { ...Success.GET, vehiculos: vehiculosPorEmpleado.rows };
+      }
+      return { ...Fail.NOT_FOUND };
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    static crearVehiculo = async (vehiculo: Vehiculo) => {
-        try {
-            const crearVehiculo = await pool.query(
-                `
+  static crearVehiculo = async (vehiculo: Vehiculo) => {
+    try {
+      const crearVehiculo = await pool.query(
+        `
             INSERT INTO vehiculo (dominio, marca, modelo, anio, kilometraje, id_empleado, estado) 
             VALUES ($1, $2, $3, $4, $5, $6, $7)
             `,
-                [
-                    vehiculo.dominio,
-                    vehiculo.marca,
-                    vehiculo.modelo,
-                    vehiculo.anio,
-                    vehiculo.kilometraje,
-                    vehiculo.id_empleado,
-                    vehiculo.estado,
-                ]
-            );
+        [
+          vehiculo.dominio,
+          vehiculo.marca,
+          vehiculo.modelo,
+          vehiculo.anio,
+          vehiculo.kilometraje,
+          vehiculo.id_empleado,
+          vehiculo.estado,
+        ]
+      );
 
-            if (!!crearVehiculo.rowCount) return { ...Success.CREATION };
+      if (!!crearVehiculo.rowCount) return { ...Success.CREATION };
 
-            return { ...Fail.CREATION };
-        } catch (error) {
-            throw error;
-        }
-    };
+      return { ...Fail.CREATION };
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    static editarVehiculo = async (vehiculo: Vehiculo, idVehiculo: number) => {
-        try {
-            const editarVehiculo = await pool.query(
-                `
-                UPDATE vehiculo SET dominio = $1, marca = $2, anio = $3, kilometraje = $4, id_empleado = $5, estado = $6 WHERE vehiculo.id = $7
+  static editarVehiculo = async (vehiculo: Vehiculo, idVehiculo: number) => {
+    try {
+      const editarVehiculo = await pool.query(
+        `
+                UPDATE vehiculo SET dominio = $1, marca = $2, modelo = $3, anio = $4, kilometraje = $5, id_empleado = $6, estado = $7 WHERE vehiculo.id = $8
                 `,
-                [
-                    vehiculo.dominio,
-                    vehiculo.marca,
-                    vehiculo.anio,
-                    vehiculo.kilometraje,
-                    vehiculo.id_empleado,
-                    vehiculo.estado,
-                    idVehiculo,
-                ]
-            );
+        [
+          vehiculo.dominio,
+          vehiculo.marca,
+          vehiculo.modelo,
+          vehiculo.anio,
+          vehiculo.kilometraje,
+          vehiculo.id_empleado,
+          vehiculo.estado,
+          idVehiculo,
+        ]
+      );
 
-            if (editarVehiculo.rowCount) {
-                return { ...Success.EDIT };
-            } else {
-                return Fail.NOT_FOUND;
-            }
-        } catch (error) {
-            throw error;
-        }
-    };
+      if (editarVehiculo.rowCount) {
+        return { ...Success.EDIT };
+      } else {
+        return Fail.NOT_FOUND;
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    static eliminarVehiculo = async (idVehiculo: number) => {
-        try {
+  static eliminarVehiculo = async (idVehiculo: number) => {
+    try {
+      const eliminarRelacion = await ViajeModel.eliminarViaje(
+        idVehiculo,
+        "id_vehiculo"
+      );
 
-            const eliminarRelacion = await ViajeModel.eliminarViaje(idVehiculo,'id_vehiculo')
+      const eliminarVehiculo = await pool.query(
+        `DELETE FROM vehiculo WHERE id = $1`,
+        [idVehiculo]
+      );
 
-            const eliminarVehiculo = await pool.query(
-                `DELETE FROM vehiculo WHERE id = $1`,
-                [idVehiculo]
-            );
-
-            if (eliminarVehiculo.rowCount && eliminarRelacion.CODE === 200) {
-                return { ...Success.DELETE };
-            } else {
-                return Fail.NOT_FOUND;
-            }
-        } catch (error) {
-            throw error;
-        }
-    };
+      if (eliminarVehiculo.rowCount && eliminarRelacion.CODE === 200) {
+        return { ...Success.DELETE };
+      } else {
+        return Fail.NOT_FOUND;
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
 }
