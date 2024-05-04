043ff16eff8012f4e518154cef6fec6ef482ceab
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-05-04 12:21:46 -0300

    Se agrega configuración para el deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use("/api/empleados", empleadosRouter);
 app.use("/api/vehiculos", vehiculosRouter);
