fe849c23cb9645e4b57ca00a97f699c4a0fa8173
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-05-04 18:26:47 -0300

    Se agrega el vercel.json

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,6 @@ app.use("/api/empleados", empleadosRouter);
 app.use("/api/vehiculos", vehiculosRouter);
 app.use("/api/viajes", viajesRouter);
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
-});
-
 app.listen(PORT, () => {
   console.log("SERVIDOR FUNCIONANDO.");
 });
