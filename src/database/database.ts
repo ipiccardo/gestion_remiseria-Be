dfeed97f3f658c256c9d9baa05d9a8bb8fc9be86
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-05-05 10:44:03 -0300

    Configuración para deploy

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,20 +1,11 @@
-import pg from 'pg'
-import { config } from '../utils/types'
-const { Pool } = pg
+import pg from "pg";
+const { Pool } = pg;
 import "dotenv/config";
 
+const config: { [key: string]: string } = {
+  connectionString: process.env.DATABASE_URL || "",
+};
 
-const config: config = {
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    password: process.env.PG_PASS,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE
-} 
+const pool = new Pool(config);
 
-
-const pool = new Pool(config)
-
-export default pool
-
- 
+export default pool;
